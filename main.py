7decc3da91e3a5a93fcc58b26887947ceb761842
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-10-16 19:18:02 +0200

    Creacion del proyecto

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+def chatbot():
+    print("Hola: yo soy un chatbot")
+    while True:
+        #Le pide al usuario que ingrese un mensaje
+        user_input =input("Tu: ").lower()
+        if user_input == "exit":
+            print("Chatbot: ¡Adiós!")
+            break
+        else:
+            print("Chatbot: No se como responder a eso.")
