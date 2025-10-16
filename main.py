feb314602cbbc22ae196438ebbed7ce3db11a4fd
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-10-16 19:34:45 +0200

    Finalizacion de chatbox basico

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 responses = {
     "hola": "¡Hola! ¿Cómo puedo ayudarte?",
     "¿cómo estás?": "Estoy bien, gracias por preguntar.",
@@ -6,18 +5,32 @@ responses = {
     "adiós": "¡Adiós! Que tengas un buen día."
 }
 
+
+def get_response(user_input):
+    user_input = user_input.lower()
+    if "hola" in user_input:
+        return "¡Hola! ¿Cómo puedo ayudarte?"
+    elif "cómo estás" in user_input:
+        return "Estoy bien, gracias por preguntar."
+    elif "qué puedes hacer" in user_input:
+        return "Puedo responder a tus preguntas y ayudarte con información."
+    elif "adiós" in user_input:
+        return "¡Adiós! Que tengas un buen día."
+    else:
+        return "No se como responder a eso."
+
+
 def chatbot():
     print("Hola: yo soy un chatbot")
     while True:
-        #Le pide al usuario que ingrese un mensaje
-        user_input =input("Tu: ").lower()
-        if user_input == "exit":
+        # Le pide al usuario que ingrese un mensaje
+        user_input = input("Tu: ").lower()
+        if "exit" in user_input:
             print("Chatbot: ¡Adiós!")
             break
-        elif user_input in responses:
-            print("Chatbot: " + responses[user_input])
-        else:
-            print("Chatbot: No se como responder a eso.")
+        responses = get_response(user_input)
+        print(f"chatbot: {responses}")
 
 
-chatbot()
+if __name__ == "__main__":
+    chatbot()
